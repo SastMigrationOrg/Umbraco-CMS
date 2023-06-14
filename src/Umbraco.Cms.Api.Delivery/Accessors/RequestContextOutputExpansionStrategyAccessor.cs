f0b7c4500b62d18b0e7cd9cb8c116841bab0fbf0
migration-sast <136586733+migration-sast@users.noreply.github.com>
2023-06-14 17:48:07 +0200

    Update RequestContextOutputExpansionStrategyAccessor.cs

--- src/Umbraco.Cms.Api.Delivery/Accessors/RequestContextOutputExpansionStrategyAccessor.cs
+++ src/Umbraco.Cms.Api.Delivery/Accessors/RequestContextOutputExpansionStrategyAccessor.cs
@@ -3,6 +3,7 @@ using Umbraco.Cms.Core.DeliveryApi;
 
 namespace Umbraco.Cms.Api.Delivery.Accessors;
 
+
 internal sealed class RequestContextOutputExpansionStrategyAccessor : RequestContextServiceAccessorBase<IOutputExpansionStrategy>, IOutputExpansionStrategyAccessor
 {
     public RequestContextOutputExpansionStrategyAccessor(IHttpContextAccessor httpContextAccessor)
